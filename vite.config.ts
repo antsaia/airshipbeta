a655490e14d2827c8c11f17c21e8b1990370ef56
antsaia <anthony.saia@gmail.com>
2025-05-30 16:07:10 -0700

    Update Vite configuration for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "https://antsaia.github.io/airshipbeta/",
+  base: "./",
   plugins: [react()],
   build: {
     outDir: 'dist',
