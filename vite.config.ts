856bc05e3f408dd5481e27e6de2732c04f9cca07
antsaia <anthony.saia@gmail.com>
2025-05-30 15:56:10 -0700

    Deploy to Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/airshipbeta/',
   plugins: [react()],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-      output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
+    assetsDir: 'assets'
   }
 })
