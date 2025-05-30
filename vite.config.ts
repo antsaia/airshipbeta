50280a3a979f6f8f3298df54b6c4703d4775914a
antsaia <anthony.saia@gmail.com>
2025-05-30 14:08:41 -0700

    Fix GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/airshipbeta/',
   plugins: [react()],
@@ -9,5 +8,10 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: true,
-  },
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
