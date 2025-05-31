57f88a5f4b3368c11d1cd1f69dac46ea7b15c77a
antsaia <anthony.saia@gmail.com>
2025-05-30 19:10:41 -0700

    Update Vite configuration for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
+  base: '/airshipbeta/',
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
     assetsDir: 'assets',
     rollupOptions: {
       output: {
