d3912e7fe78e3f63ecfaad3395ffe4860b067547
antsaia <anthony.saia@gmail.com>
2025-05-30 23:58:26 -0700

    Fix GitHub Pages Asset Paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/airshipbeta/' : '/',
+  base: '/airshipbeta/',
   plugins: [react()],
   build: {
     outDir: 'docs',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
+    assetsDir: 'assets'
   }
 })
