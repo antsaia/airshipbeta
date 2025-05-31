61d3c7df24248b04d4922653f204866c0a9c26c0
antsaia <anthony.saia@gmail.com>
2025-05-31 00:22:52 -0700

    Update GitHub Pages configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'docs',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 })
